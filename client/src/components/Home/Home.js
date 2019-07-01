6e1ac021fea28dc4c39525b5f42a43a27c1867a3
Daria <dariaurbanik@gmail.com>
2019-07-01 13:53:50 +0200

    Responsive styling

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import '../Home/Home.css';
 import couple from '../../public/img/couple.jpg';
+import thunder from '../../public/img/thunder.png';
 
 class Home extends Component {
     render() {
         return (
             <div>
-                <img src={couple} alt="Couple"
+                <img className="hero" src={couple} alt="Couple"
                 />
                 <div className="hero-text">
                 <h1>The BreakUp</h1>
-                <h3>Welcome to Breakup Service, your break up facilitator.</h3>
-                <p>You made the decition - great! Let us do the rest!</p>
+                <h3>Welcome to BreakUp Service, your break up facilitator.</h3>
+                <p>You made the decision - great! Let us do the rest!</p>
                 </div>
+                <img className="icon" src={thunder} alt="Lightening" />
             </div>
         )
     }
